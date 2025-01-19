016621ea152ed160564d23c82d39a0efd93700b0
sucooer <cocooer@mail.ru>
2025-01-19 20:10:20 +0800

    移除未使用的 DEFAULT_ICONS 常量

--- index.js
+++ index.js
@@ -40,22 +40,6 @@ hexo.extend.generator.register('panlink-icons', function (locals) {
   }
 });
 
-// 平台图标映射（使用 Base64 编码的默认图标）
-const DEFAULT_ICONS = {
-  'baidu': `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="#2196F3"/>
-    <path d="M12 6c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm0 10c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z" fill="#2196F3"/>
-  </svg>`,
-  '360': `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-    <circle cx="12" cy="12" r="10" fill="#4285F4"/>
-    <path d="M12 6c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm0 10c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z" fill="#FFFFFF"/>
-  </svg>`,
-  'lanzou': `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="#00BCD4"/>
-    <circle cx="12" cy="12" r="4" fill="#00BCD4"/>
-  </svg>`
-};
-
 hexo.extend.tag.register('panlink', function (args) {
   try {
     const processedArgs = args.map(arg => arg.replace(/^["']|["']$/g, ''));
